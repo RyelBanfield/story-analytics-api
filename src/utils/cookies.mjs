cd7f58b23cf7e9dd805e6433a0bb532f4fa670da
Ryel Banfield <ryelbanfield@gmail.com>
2023-02-10 14:12:44 -0400

    Add console.log

--- src/utils/cookies.mjs
+++ src/utils/cookies.mjs
@@ -7,18 +7,22 @@ dotenv.config();
 const Login = async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(0);
 
   await page.goto("https://www.instagram.com/accounts/login/");
+  console.log("Page loaded");
 
   await page.waitForSelector('input[name="username"]');
   await page.type('input[name="username"]', process.env.INSTA_USERNAME);
   await page.type('input[name="password"]', process.env.INSTA_PASSWORD);
   await page.click('button[type="submit"]');
+  console.log("Logged in");
+
   await page.waitForNavigation();
+  console.log("Page loaded");
 
   const cookies = await page.cookies();
   await fs.writeFile("./cookies.json", JSON.stringify(cookies, null, 2));
+  console.log("Cookies saved");
 
   await browser.close();
 };
