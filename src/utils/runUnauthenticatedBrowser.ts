fbf61ee452b76919c14c0e56731d7005e6c14b7e
Ryel Banfield <ryelbanfield@gmail.com>
2023-02-10 13:15:54 -0400

    Add args

--- src/utils/runUnauthenticatedBrowser.ts
+++ src/utils/runUnauthenticatedBrowser.ts
@@ -2,7 +2,9 @@ import fs from "fs/promises";
 import puppeteer from "puppeteer";
 
 const runUnauthenticatedBrowser = async (url: string): Promise<boolean> => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+  });
   const page = await browser.newPage();
 
   const cookiesString = await fs.readFile("./cookies.json");
