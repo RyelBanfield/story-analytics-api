bcb6b50c7fcb0dc4dd40d8654b3e847a0b32baf7
Ryel Banfield <ryelbanfield@gmail.com>
2023-02-10 01:39:59 -0400

    Create bot

---
+++ src/utils/runUnauthenticatedBrowser.ts
@@ -0,0 +1,28 @@
+import fs from "fs/promises";
+import puppeteer from "puppeteer";
+
+const runUnauthenticatedBrowser = async (url: string): Promise<boolean> => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  const cookiesString = await fs.readFile("./cookies.json");
+  const cookies = JSON.parse(cookiesString as unknown as string) as JSON[];
+  await page.setCookie(...(cookies as []));
+
+  await page.goto("https://www.instagram.com/accounts/login/");
+
+  const newPage = await browser.newPage();
+  await newPage.goto(url);
+  await newPage.waitForSelector("h2");
+  const h2 = await newPage.$eval("h2", (el) => el.textContent);
+
+  await browser.close();
+
+  if (h2 === "Sorry, this page isn't available.") {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+export default runUnauthenticatedBrowser;
