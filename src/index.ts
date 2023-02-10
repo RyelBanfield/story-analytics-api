dfb8bb89d16fb5934fcb6c6cf97f2b6a27dbbb8d
Ryel Banfield <ryelbanfield@gmail.com>
2023-02-10 01:40:12 -0400

    Finish implementation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
 
+import runUnauthenticatedBrowser from "./utils/runUnauthenticatedBrowser.js";
+
 const app = express();
 const port = 3001;
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -13,9 +17,16 @@ app.use((req, res, next) => {
 });
 
 app.post("/", (req, res) => {
-  res.send({ postExists: true });
+  const { url } = req.body as { url: string };
+
+  runUnauthenticatedBrowser(url)
+    .then((postExists) => res.status(200).json({ postExists }))
+    .catch((err: Error) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
