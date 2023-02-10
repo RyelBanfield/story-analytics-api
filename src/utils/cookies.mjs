b45a310fd0a65b2835e0867e8df954dec35052a9
Ryel Banfield <ryelbanfield@gmail.com>
2023-02-10 00:51:18 -0400

    Install puppeteer and build cookie script

---
+++ src/utils/cookies.mjs
@@ -0,0 +1,25 @@
+import * as dotenv from "dotenv";
+import fs from "fs/promises";
+import puppeteer from "puppeteer";
+
+dotenv.config();
+
+const Login = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto("https://www.instagram.com/accounts/login/");
+
+  await page.waitForSelector('input[name="username"]');
+  await page.type('input[name="username"]', process.env.INSTA_USERNAME);
+  await page.type('input[name="password"]', process.env.INSTA_PASSWORD);
+  await page.click('button[type="submit"]');
+  await page.waitForNavigation();
+
+  const cookies = await page.cookies();
+  await fs.writeFile("./cookies.json", JSON.stringify(cookies, null, 2));
+
+  await browser.close();
+};
+
+await Login();
