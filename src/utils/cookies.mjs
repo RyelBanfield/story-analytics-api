eb13c47e238ac2ed05309b614cae8eef58b05dac
Ryel Banfield <ryelbanfield@gmail.com>
2023-02-10 13:47:26 -0400

    Fix waitForNav

--- src/utils/cookies.mjs
+++ src/utils/cookies.mjs
@@ -5,7 +5,7 @@ import puppeteer from "puppeteer";
 dotenv.config();
 
 const Login = async () => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   await page.goto("https://www.instagram.com/accounts/login/");
@@ -14,7 +14,7 @@ const Login = async () => {
   await page.type('input[name="username"]', process.env.INSTA_USERNAME);
   await page.type('input[name="password"]', process.env.INSTA_PASSWORD);
   await page.click('button[type="submit"]');
-  await page.waitForNavigation();
+  await page.waitForNavigation({ waitUntil: "networkidle0" });
 
   const cookies = await page.cookies();
   await fs.writeFile("./cookies.json", JSON.stringify(cookies, null, 2));
