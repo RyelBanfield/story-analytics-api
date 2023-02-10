4cdebb5af8e6d6b9f764989f4ca107f6bbabb840
Ryel Banfield <ryelbanfield@gmail.com>
2023-02-10 14:35:54 -0400

    Add timeout

--- src/utils/cookies.mjs
+++ src/utils/cookies.mjs
@@ -7,6 +7,7 @@ dotenv.config();
 const Login = async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
 
   await page.goto("https://www.instagram.com/accounts/login/");
 
@@ -14,8 +15,7 @@ const Login = async () => {
   await page.type('input[name="username"]', process.env.INSTA_USERNAME);
   await page.type('input[name="password"]', process.env.INSTA_PASSWORD);
   await page.click('button[type="submit"]');
-
-  await page.waitForNavigation({ waitUntil: "networkidle2" });
+  await page.waitForNavigation();
 
   const cookies = await page.cookies();
   await fs.writeFile("./cookies.json", JSON.stringify(cookies, null, 2));
